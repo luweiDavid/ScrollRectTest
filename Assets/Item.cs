7f755552dfcb581e79a96352a912dd96d3790b98
luweiDavid <daiwei@talkmoney.cn>
2019-03-25 19:01:51 +0800

    create program

---
+++ Assets/Item.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item { 
+    public GameObject itemGo;
+
+    private Text nameTxt;
+    private Text ageTxt;
+
+    public Item(GameObject go) { 
+        this.itemGo = go;
+
+        if (itemGo) {
+            nameTxt = itemGo.transform.Find("NameTxt").GetComponent<Text>();
+            ageTxt = itemGo.transform.Find("AgeTxt").GetComponent<Text>();
+        }
+    }
+
+    public void SetData(ItemData data) {
+        if (data != null) {
+            nameTxt.text = data.m_Name;
+            ageTxt.text = data.m_Age.ToString();
+        }  
+    }
+}
+
+public class ItemData {
+    private string Name = string.Empty;
+    public string m_Name {
+        get {
+            return this.Name;
+        }
+    }
+    private int Age = 0;
+    public int m_Age {
+        get {
+            return this.Age;
+        }
+    }
+
+    public ItemData(string name, int age) {
+        this.Name = name;
+        this.Age = age;
+    }
+}
---
+++ Assets/ScrollerViewTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollerViewTest : MonoBehaviour {
+
+    public ScrollRect m_scrollRect;
+    private RectTransform contentRectTr;
+
+    private void Start()
+    {
+        if (m_scrollRect != null) {
+            contentRectTr = m_scrollRect.transform.Find("Content").GetComponent<RectTransform>();
+
+        }
+    }
+
+}
